075332e5653828c1f7f0bae6d237560893f3563b
thundermiracle <cissoliu@gmail.com>
2025-07-20 12:50:50 +0900

    fix: validation of postal code

--- app/backend/src/presentation/common/validators/postal_code.rs
+++ app/backend/src/presentation/common/validators/postal_code.rs
@@ -5,27 +5,26 @@ use validator::ValidationError;
 /// 日本の郵便番号バリデーター関数
 /// 
 /// 以下の形式に対応:
-/// - 1234567 (7桁の数字)
 /// - 123-4567 (3桁-4桁の形式)
 /// 
 /// # Examples
 /// ```
 /// use crate::presentation::validators::validate_japanese_postal_code;
 /// 
-/// assert!(validate_japanese_postal_code("1234567").is_ok());
 /// assert!(validate_japanese_postal_code("123-4567").is_ok());
+/// assert!(validate_japanese_postal_code("1234567").is_err());
 /// assert!(validate_japanese_postal_code("12345").is_err());
 /// ```
 pub fn validate_japanese_postal_code(value: &str) -> Result<(), ValidationError> {
     static POSTAL_CODE_REGEX: LazyLock<Regex> = LazyLock::new(|| {
-        Regex::new(r"^\d{3}-?\d{4}$").unwrap() // ハイフンありなしどちらも対応
+        Regex::new(r"^\d{3}-\d{4}$").unwrap() // ハイフンありのみ対応
     });
     
     if POSTAL_CODE_REGEX.is_match(value) {
         Ok(())
     } else {
         let mut error = ValidationError::new("invalid_postal_code");
-        error.message = Some("郵便番号は7桁の数字またはXXX-XXXX形式で入力してください".into());
+        error.message = Some("郵便番号はXXX-XXXX形式で入力してください".into());
         Err(error)
     }
 }
